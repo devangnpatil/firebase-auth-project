cad9a0f61fee6a0918a50acc7e535b74547da985
devang <devang.patil@upgrad.com>
2019-04-16 23:19:26 +0530

    firebase function chnage

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 firebase-auth-project
 
 Hosting URL: https://admin-project-ca154.firebaseapp.com
+
+Deploy function
+firebase deploy --only functions
--- functions/index.js
+++ functions/index.js
@@ -4,18 +4,16 @@ admin.initializeApp();
 
 exports.addAdminRole = functions.https.onCall((data, context)=>{
     // get user and custom claim (admin)
-    admin.auth().getUserByEmail(data.email).then(user =>{
-        console.log('===>', user);
+    return admin.auth().getUserByEmail(data.email).then(user =>{
         return admin.auth().setCustomUserClaims(user.uid, {
             admin: true
         });
-    }).then((data) =>{
-        console.log('Success');
+    }).then(() =>{ 
         return {
             message: `Success ${data.email} has been made admin.`
         }
     }).catch(err => {
+        console.log('Fail');
         return err;
-    })
-
-})
+    });
+});
